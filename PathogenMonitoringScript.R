af49f592f920cc8b71becabf99456df7e8f19994
aliczner <aliczner@gmail.com>
2021-03-18 20:41:35 -0400

    changed resolution to 900x900m, attempted some plots

--- PathogenMonitoringScript.R
+++ PathogenMonitoringScript.R
@@ -2,9 +2,11 @@
 
 ##libraries
 library(raster)
+library(rgdal) #for spTransform
 
 ##datafiles
-CanadaAll<-raster("CanadaAllRasters.tif") #created in data prep below
+CanadaReclass<-raster("reclassed.tif")
+poly<-shapefile("CanadaPolygon.shp")
 
 ##data prep
 
@@ -19,3 +21,24 @@ writeRaster(CanadaAll, "CanadaAllRasters.tif") #writing a raster file for all Ag
 #reclassify crop classes as pollinated by managed crops or not
 reclass<-read.csv("CropReclassify.csv") #file to reclassify crop data as pollinated by managed bees
 newCanada<-reclassify(CanadaAll, reclass, filename="reclassed.tif") #reclassifies the crop classifications as 1 (for pollinated by managed bees) or 2 (for not pollinated by managed bees)
+
+##getting polygon of Canada for mapping
+
+CanadaPoly<-getData('GADM',country='CAN',level=1) #accessing shapefile for Canada
+shapefile(CanadaPoly, filename="CanadaPolygon.shp") #wriitng Canada polygon shapefile
+
+#forgot to reproject before saving polygon need to reproject the shapefile to match the ag raster 
+polynew<-spTransform(poly, crs(CanadaReclass))
+shapefile(polynew, filename="CanadaPolygon.shp", overwrite=TRUE)
+
+
+## Making plots
+
+#raw plot of where the managed crops are
+plot(CanadaReclass, col=c("#FFFFFFFF", "#533A71"), breaks=c(0,1,2), legend=FALSE)
+plot(poly, add=T)
+
+#making less fine resolution image of the map
+crop100<-raster::aggregate(CanadaReclass, fact=30) #900 x 900 m resolution, still speckled 
+crop10k<-raster::aggregate(crop100, fact=10) #9000 x 9000 resolution
+writeRaster(crop10k, "CanadaLowRes.tif")
